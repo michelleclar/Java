2808814aed242be6a4958d724a28d2aedb44ba81
Carl <carlmichelle493@gmail.com>
2024-02-18 18:45:24 +0800

    springboot

--- SpringBoot/src/main/java/org/carl/controller/HelloController.java
+++ SpringBoot/src/main/java/org/carl/controller/HelloController.java
@@ -2,6 +2,7 @@ package org.carl.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,12 @@ public class HelloController {
     @GetMapping("/welcome")
     public String welcome(){
         return "Hello World";
+    }
+
+    @GetMapping("/t")
+    public String sayHello(@RequestParam("url") String url) {
+        System.out.println(url);
+//    return restTemplate.getForObject("http://basic-provider/sayHello?name={1}", String.class, name);
+        return url;
     }
 }
