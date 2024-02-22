89bcaf0641122bc2d7a138cb40aa562c24210b62
Carl <carlmichelle493@gmail.com>
2024-02-22 22:58:03 +0800

    Quarkus

---
+++ demo/src/main/java/org/carl/GreetingService.java
@@ -0,0 +1,13 @@
+package org.carl;
+
+import io.quarkus.vertx.ConsumeEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+  @ConsumeEvent("greetings")
+  public String hello(String name) {
+    return "Hello " + name;
+  }
+}
