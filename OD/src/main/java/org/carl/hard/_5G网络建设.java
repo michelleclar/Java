9e941521cea1f7f78273082cbabf3acd9c0bd8bd
Carl <carlmichelle493@gmail.com>
2024-03-28 16:03:00 +0800

    OD

--- OD/src/main/java/org/carl/hard/_5G网络建设.java
+++ OD/src/main/java/org/carl/hard/_5G网络建设.java
@@ -1,80 +1,61 @@
 package org.carl.hard;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class _5G网络建设 {
   static Scanner in = new Scanner(System.in);
 
+  /*
+   * 3 3 1 2 7 0 1 3 1 0 2 3 5 0
+   */
   public static void main(String[] args) {
     int N = in.nextInt();
     int M = in.nextInt();
-    Map<Integer, Base> map = new HashMap<>();
+    List<Edgs> lEdgs = new ArrayList<>();
+    List<Integer> list = new ArrayList<>();
     for (int i = 0; i < M; i++) {
       int X = in.nextInt();
       int Y = in.nextInt();
       int Z = in.nextInt();
       int P = in.nextInt();
-      Base b = get(map, X);
-      Base b2 = get(map, Y);
-      Pair<Integer, Base> pair = new Pair<>();
-      pair.k = Z;
-      pair.v = b2;
-      b.l.add(pair);
-      if (P == 1) b.con.add(b2);
+      Edgs e = new Edgs(X, Y, Z);
+      lEdgs.add(e);
+      if (P == 1) list.addAll(Arrays.asList(X, Y));
     }
+    Set<Integer> set = new HashSet<>(list);
+    lEdgs.sort(
+        (o1, o2) -> {
+          return o1.l - o2.l;
+        });
+    int result = 0;
+    for (Edgs e : lEdgs) {
+      if (set.contains(e.a) && set.contains(e.b)) {
+        // 有环
+        continue;
+      }
+      result += e.l;
+      set.add(e.a);
+
+      set.add(e.b);
+    }
+    if (set.size() == N) System.out.println(result);
+    else System.out.println(-1);
   }
 
-  static Base get(Map<Integer, Base> map, int i) {
-    Base b = map.get(i);
-    return b == null ? new Base(i) : b;
-  }
+  static class Edgs {
+    Integer a;
+    Integer b;
+    Integer l;
 
-  static class Base {
-    Integer i;
-    List<Pair<Integer, Base>> l = new ArrayList<Pair<Integer, Base>>();
-    List<Base> con;
-
-    Base() {}
-
-    Base(int i) {
-      this.i = i;
-    }
-
-    @Override
-    public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((i == null) ? 0 : i.hashCode());
-      return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (this == obj) return true;
-      if (obj == null) return false;
-      if (getClass() != obj.getClass()) return false;
-      Base other = (Base) obj;
-      if (i == null) {
-        if (other.i != null) return false;
-      } else if (!i.equals(other.i)) return false;
-      return true;
-    }
-  }
-
-  static class Pair<K, V> {
-    K k;
-    V v;
-
-    public K getK() {
-      return k;
-    }
-
-    public V getV() {
-      return v;
+    public Edgs(Integer a, Integer b, Integer l) {
+      this.a = a;
+      this.b = b;
+      this.l = l;
     }
   }
 }
---
+++ OD/src/main/java/org/carl/hard/可以组成网络的服务器.java
@@ -0,0 +1,36 @@
+package org.carl.hard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class 可以组成网络的服务器 {
+  static Scanner in = new Scanner(System.in);
+  static int n;
+  static int m;
+
+  public static void main(String[] args) {
+    int n = in.nextInt();
+    int m = in.nextInt();
+    int[][] arr = new int[n][m];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        arr[i][j] = in.nextInt();
+      }
+    }
+    List<Integer> results = new ArrayList<Integer>();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (arr[i][j] == 1) results.add(calc(i, j, arr, 1));
+      }
+    }
+  }
+
+  static int calc(int i, int j, int[][] arr, int result) {
+    if (i < n && j < m) return result;
+
+    if (arr[i][j] == 0) return result;
+    if (arr[i][j] == 1) {}
+    return result;
+  }
+}
