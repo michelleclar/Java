97520999eebbb21bce8365a4e29a2d34bffa4c70
Carl <carlmichelle493@gmail.com>
2024-04-03 19:03:42 +0800

    OD

---
+++ OD/src/main/java/org/carl/hard/最长子字符串的长度.java
@@ -0,0 +1,15 @@
+package org.carl.hard;
+
+import java.util.Scanner;
+
+public class 最长子字符串的长度 {
+  static Scanner in = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    String line = in.nextLine();
+    for (int i = 0; i < line.length(); i++) {
+
+      
+    }
+  }
+}
