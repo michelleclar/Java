2870be8dd53bc4b95f3e4ab73aac5917ff510d51
Carl <carlmichelle493@gmail.com>
2024-08-07 16:23:11 +0800

    add static of method

--- common/src/main/java/org/carl/commons/struct/Pair.java
+++ common/src/main/java/org/carl/commons/struct/Pair.java
@@ -1,42 +1,46 @@
 package org.carl.commons.struct;
 
 public class Pair<F, S> {
-  private final F first;
-  private final S second;
+    private final F first;
+    private final S second;
 
-  public Pair(F first, S second) {
-    this.first = first;
-    this.second = second;
-  }
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
 
-  public F getFirst() {
-    return first;
-  }
+    public F getFirst() {
+        return first;
+    }
 
-  public S getSecond() {
-    return second;
-  }
+    public S getSecond() {
+        return second;
+    }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null || getClass() != obj.getClass()) return false;
+    public static <F, S> Pair<F, S> of(F first, S second) {
+        return new Pair<>(first, second);
+    }
 
-    Pair<?, ?> pair = (Pair<?, ?>) obj;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
 
-    if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
-    return second != null ? second.equals(pair.second) : pair.second == null;
-  }
+        Pair<?, ?> pair = (Pair<?, ?>) obj;
 
-  @Override
-  public int hashCode() {
-    int result = first != null ? first.hashCode() : 0;
-    result = 31 * result + (second != null ? second.hashCode() : 0);
-    return result;
-  }
+        if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+        return second != null ? second.equals(pair.second) : pair.second == null;
+    }
 
-  @Override
-  public String toString() {
-    return "(" + first + ", " + second + ")";
-  }
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (second != null ? second.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
 }
