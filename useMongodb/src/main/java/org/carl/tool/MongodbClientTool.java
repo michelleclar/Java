8d92cfc5b445a8f6fea5daa05ea4a8e86193514c
Carl <carlmichelle493@gmail.com>
2024-10-23 08:45:49 +0800

    mongodb

--- useMongodb/src/main/java/org/carl/tool/MongodbClientTool.java
+++ useMongodb/src/main/java/org/carl/tool/MongodbClientTool.java
@@ -4,7 +4,6 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
